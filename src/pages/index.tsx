15ce3a43784dbd544feaf376e3f95e73c70dd594
Ryosuke Asano <ryosukeusuusu@gmail.com>
2024-04-17 16:07:58 +0900

    ディレクションによる爆散攻撃を実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
+const directions = [
+  [0, 0],
+  [0, -1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+  [0, 1],
+  [-1, 1],
+  [-1, 0],
+  [-1, -1],
+];
+
 const Home = () => {
+  const [turnColor, setTurn] = useState(1);
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,16 +26,47 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
+  const clickHandler = (x: number, y: number) => {
+    const newBoard = structuredClone(board);
+
+    directions.map((aDirectionArray) => {
+      let log = [];
+      let targetStonePosition = [y, x];
+
+      let i = 0;
+      while (i < 6) {
+        targetStonePosition = [
+          targetStonePosition[0] + aDirectionArray[0],
+          targetStonePosition[1] + aDirectionArray[1],
+        ];
+
+        if (
+          board[targetStonePosition[0]] !== undefined &&
+          board[targetStonePosition[0]][targetStonePosition[1]] !== undefined
+        ) {
+          newBoard[targetStonePosition[0]][targetStonePosition[1]] = turnColor;
+          setTurn(3 - turnColor);
+          i++;
+        } else {
+          targetStonePosition = [x, y];
+          break;
+        }
+      }
+    });
+
+    setBoard(newBoard);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.boardStyle}>
         {board.map((row, y) =>
           row.map((color, x) => (
-            <div className={styles.cell} key={`${x}-${y}`}>
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
               {color !== 0 && (
                 <div
                   className={styles.stone}
-                  style={{ background: color === 1 ? "#000" : "#FFFFF" }}
+                  style={{ background: color === 1 ? 'black' : 'white' }}
                 />
               )}
             </div>
